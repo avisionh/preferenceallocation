82e6b0c37b039c2e2840a2211afdc4a2ff6c850e
A Ho <avision.ho@education.gov.uk>
2018-12-16 20:37:21 +0000

    fix: update directory where you get HTML file

--- _deploy.sh
+++ _deploy.sh
@@ -16,7 +16,7 @@ git clone -b gh-pages \
   book-output
 
 # Copy locally built *.html files into 
-cp -r ../index.html ./ 
+cp -r index.html book-output/
 
 # Create .nojekyll file to prevent git from trying to build
 # html pages with jekyll.
