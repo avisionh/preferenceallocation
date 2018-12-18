071438d4f3483e444babb36db4e5739ba40fb789
Avision Ho <avision.ho@education.gov.uk>
2018-12-18 21:59:07 +0000

    chore: create basic script for dynamic objects.
    This is to start us off with creating dynamically-changing objects such as tables, plots and text that updates according to a user's input.

---
+++ server.R
@@ -0,0 +1,12 @@
+# -------- #
+# server.R #
+# -------- #
+
+# DESC: server.R code can be much longer and complicated than in ui.R.
+#       Is where all the dynamic data manipulation happens, and plot creations.
+#       e.g. Filtering based on user inputs and generating plots based on dynamically filtered data.
+#       server.R must create a function called server, like below:
+
+server <- function(input, output, session) {
+  
+}
