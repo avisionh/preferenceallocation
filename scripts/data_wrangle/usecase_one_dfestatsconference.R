b478cc2cacbd059b391be8bee8dcc6805f619e46
Avision Ho <avision.ho@education.gov.uk>
2019-08-15 11:23:32 +0100

    feat: load data and reformat dataset.
    This is to start focusing on the right parts of the data and to remove unecessary bits.

--- scripts/data_wrangle/usecase_one_dfestatsconference.R
+++ scripts/data_wrangle/usecase_one_dfestatsconference.R
@@ -10,4 +10,20 @@
 library(readr)
 library(dplyr)
 library(stringr)
+library(tidyr)
 
+# global variables
+
+# import data
+data <- read_csv(file = "./data/usecase_one_dfestatsconference.csv")
+
+# 1. rename columns
+colnames_old <- colnames(data)
+colnames_new <- str_replace(string = colnames_old, pattern = "ParallelSessions_", replacement = "")
+data <- data %>% 
+  # rename columns
+  rename_at(.vars = vars(colnames_old), .funs = function(x) colnames_new) %>% 
+  # select relevant columns
+  select(ID, FirstChoice:FifthChoice5) %>% 
+  # convert to long format
+  gather(key = "ID", value = )
