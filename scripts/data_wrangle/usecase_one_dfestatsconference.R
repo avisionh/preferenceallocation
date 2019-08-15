71731ecd356b65dae71b131291b401f089555d14
Avision Ho <avision.ho@education.gov.uk>
2019-08-15 11:04:54 +0100

    chore: save anonymised data file and draft script.
    This script just contains basic information on its purpose and what it does.

---
+++ scripts/data_wrangle/usecase_one_dfestatsconference.R
@@ -0,0 +1,13 @@
+# -------------------------------- #
+# usecase_one_dfestatsconference.R #
+# -------------------------------- #
+# DESCRIPTION
+# This script wrangles an anonymised dataset of delegates' preferences for 
+# the DfE Statistician's Conference for 2019.
+# The survey/form used to collect the responses is from:
+# https://www.cognitoforms.com/
+
+library(readr)
+library(dplyr)
+library(stringr)
+
