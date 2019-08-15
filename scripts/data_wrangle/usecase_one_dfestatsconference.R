4193b792a474df359d9c65fd0d49f2fba5a2456b
Avision Ho <avision.ho@education.gov.uk>
2019-08-15 18:34:31 +0100

    feat: add example of formatted dataset for algorithm.
    There are still issues to deal with such as the same session having different preferences orderings for the same individual (see ID = 27) and NAs throwing the algorithm off course so these need to be resolved too but example is a starter for how it can work.

--- scripts/data_wrangle/usecase_one_dfestatsconference.R
+++ scripts/data_wrangle/usecase_one_dfestatsconference.R
@@ -15,12 +15,12 @@ library(tidyr)
 # global variables
 
 # import data
-data <- read_csv(file = "./data/usecase_one_dfestatsconference.csv")
+data_core <- read_csv(file = "./data/usecase_one_dfestatsconference.csv")
 
 # 1. reformat data for analysis
-colnames_old <- colnames(data)
+colnames_old <- colnames(data_core)
 colnames_new <- str_replace(string = colnames_old, pattern = "ParallelSessions_", replacement = "")
-data <- data %>% 
+data <- data_core %>% 
   # rename columns
   rename_at(.vars = vars(colnames_old), .funs = function(x) colnames_new) %>% 
   # select relevant columns
@@ -36,18 +36,29 @@ rm(colnames_old, colnames_new)
 
 # 2. convert preference string to utility integers
 data <- data %>%
-  # generalise Preference column by removing part of string after "Choice"
-  mutate(Preference = str_replace(string = Preference, pattern = "Choice.*", ""),
-         Preference = case_when(
-           Preference == "First"    ~ 5,
-           Preference == "Second"   ~ 4,
-           Preference == "Third"    ~ 3,
-           Preference == "Fourth"   ~ 2,
-           Preference == "Fifth"    ~ 1,
-           TRUE                     ~ NA_real_
-         )
+  # generalise Preference column
+  mutate(# part of string after "Choice"
+    Preference = str_replace(string = Preference, pattern = "Choice.*", ""),
+    Preference = case_when(
+      Preference == "First"    ~ 5,
+      Preference == "Second"   ~ 4,
+      Preference == "Third"    ~ 3,
+      Preference == "Fourth"   ~ 2,
+      Preference == "Fifth"    ~ 1,
+      TRUE                     ~ NA_real_
+      ),
+    # extract decimal part
+    Session = as.double(str_extract(string = Session, pattern = "\\d+\\.*\\d*"))
   )
-         ))
 
-
+# 3. EXAMPLE: deal with Session 5s
+data_example <- data %>% 
+  filter(floor(Session) == 5) %>% 
+  # have issues with repeated preferences and NAs which need to be resolved e.g ID = 27
+  head(4) %>% 
+  # widen on sessions so in ideal format for algo
+  spread(key = Session, value = Preference)
+room_sizes = data.frame(Room = c("S", "M", "G", "B"),
+                        Size = c(95, 90, 56, 40))
+func_iterative_preferences(x = data_example, limits = room_sizes, with_replacement = FALSE)
   
